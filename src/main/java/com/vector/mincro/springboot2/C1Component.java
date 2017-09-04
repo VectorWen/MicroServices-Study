da8ba9b8ca416a7d455c11aa87a9766e7ecb64b1
vector.huang <642378415@qq.com>
2017-09-04 10:46:45 +0800

    完成第5天的学习，希望能坚持188天。 倒数183天
    学习内容
    1. log
    2. log4j2

--- src/main/java/com/vector/mincro/springboot2/C1Component.java
+++ src/main/java/com/vector/mincro/springboot2/C1Component.java
@@ -1,5 +1,7 @@
 package com.vector.mincro.springboot2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +16,14 @@ import java.util.Date;
 public class C1Component {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private Logger log = LoggerFactory.getLogger(C3Logger.class);
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
 
         System.out.println("现在时间：" + dateFormat.format(new Date()));
+        log.debug("现在时间：" + dateFormat.format(new Date()));
+        log.info("现在时间：" + dateFormat.format(new Date()));
     }
 
 }
--- src/main/java/com/vector/mincro/springboot2/C3Logger.java
+++ src/main/java/com/vector/mincro/springboot2/C3Logger.java
@@ -1,10 +1,22 @@
 package com.vector.mincro.springboot2;
 
+import com.vector.mincro.entity.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * author: vector.huang
  * date: 2017/08/28 14:39
  */
+@RestController
+@RequestMapping("/logger")
 public class C3Logger {
+
+    /*
+        SpringBoot 使用的是CommonsLogging（slf4j） 通用日志接口，具体实现会被动态查找
+     */
 
     /* 2016-04-13 08:23:50.120  INFO 37397 --- [main]org.hibernate.Version: HHH000412: Hibernate Core {4.3.11.Final}
      * 时间日期 — 精确到毫秒
@@ -15,4 +27,15 @@ public class C3Logger {
        Logger名 — 通常使用源代码的类名
        日志内容
      */
+
+    @RequestMapping("/impl")
+    public Object impl() {
+
+        //这里可以获取一个实现
+        //org.apache.commons.logging.impl.SLF4JLocationAwareLog -- 默认实现，不含其它实现的时候
+        Logger log = LoggerFactory.getLogger(C3Logger.class);
+        log.debug("我是测试logger 的 ：" + log.getClass().getSimpleName());
+
+        return new Result(log.getClass().getSimpleName() + " : " + log.getClass().getName());
+    }
 }
